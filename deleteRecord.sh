4ee0bef61817bfb3b8f97263b7af1c27ab07ff3a
mohammedaldghar <mohammedaldghar5@gmial.com>
2022-12-28 18:00:07 +0200

    Drop table and delete records(not finfished

---
+++ deleteRecord.sh
@@ -0,0 +1,89 @@
+export LC_COLLATE=C
+shopt -s extglob
+
+declare -a colNames
+regexNum='^[0-9]+$';
+regexChar='^[a-zA-Z]+[0-9]*';
+flag=0
+declare -i colNumber
+read -p "Enter table name : " tableName
+colNames=($(sed -n -e "s/:/ /g" -e "1p" ./$tableName))
+declare -i colLength=${#colNames[@]}
+if [ ! -f $tableName ]; then
+	echo "Table is not exist";
+createDB_table.sh
+fi
+select choice in DeleteByColumnName DeleteAll Back
+do
+case $choice in
+DeleteByColumnName)
+read -p "Enter column name: " colName
+for (( i=0;i<$colLength;i++ ))
+do
+if [[ $colName == ${colNames[$i]} ]];then
+flag=1
+colNumber=$i;
+echo $colNumber;
+break;
+fi
+done
+if (( $flag==0 ));then
+echo "this column is not exist"
+else
+colNumber=$colNumber+1
+colData=($(sed -n '3,$p' ./$tableName| cut -d: -f$colNumber))
+echo "colData" ${colData[@]};
+declare -i colDataLength=${#colData[@]};
+echo "colDataLength" $colDataLength
+read -p "Enter data you want to delete: " data
+	for (( i=0;i<$colDataLength;i++ ))
+	do
+		if [[ ${colData[$i]} == $data ]];then
+			declare -i tmp=0
+			((tmp=${colData[$i]}+2));
+			if [[ $tmp == 2 ]];then
+				if [[ $regexNum =~ $data ]];then
+					sed -i "$tmp"d ./$tableName
+					echo "with if"
+					break;
+				else
+					sed -i ''/"$data"/d'' ./$tableName
+					echo "with else with temp"
+					break;
+				fi
+
+			else
+				if [[ $regexNum =~ $data ]];then
+					sed -i "$tmp"d ./$tableName
+					echo "with if"
+					break;
+				else
+					sed -i ''/"$data"/d'' ./$tableName
+					echo "with else"
+					break;
+				fi
+			fi
+
+		else
+			echo "Not Found"
+		fi
+	done
+#cut -d: -f$colNumber ./$tableName | sed -i ''/"$data"/d'' > ./$tableName
+#sed -i ''/"$data"/d'' > ./$tableName
+#grep -n $data ./$tableName
+fi
+;;
+DeleteAll)
+	sed  -i '3,$d' ./$tableName
+	break
+;;
+Back)
+break
+;;
+*)
+echo "It's not option"
+;;
+esac
+flag=0
+echo "1)DeleteByColumnName   2)DeleteAll   3)Back"
+done
